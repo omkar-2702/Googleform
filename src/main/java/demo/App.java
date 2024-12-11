396ef83b2ded60c59c89e942fbd93ef14d0841d5
omkarbhagwatwar@gmail.com <omkarbhagwatwar@gmail.com>
2024-12-11 17:44:33 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,3 +1,44 @@
+// package demo;
+// import java.net.MalformedURLException;
+
+// public class App {
+//     public void getGreeting() throws InterruptedException, MalformedURLException {
+//         System.out.println("Hello Autmation Wizards!");
+//     }
+
+//     public static void main(String[] args) throws InterruptedException, MalformedURLException {
+//         new App().getGreeting();
+//     }
+// }
+
+// package demo;
+
+// import java.net.MalformedURLException;
+
+// public class App {
+//     public void getGreeting() throws InterruptedException, MalformedURLException {
+
+//         // This is to remove unnecessary warnings from your console
+//         System.setProperty("java.util.logging.config.file", "logging.properties");
+
+//         TestCases tests = new TestCases(); // Initialize your test class
+
+//         // TODO: call your test case functions one after other here
+
+//         tests.testCase01();
+//         tests.testCase02();
+//         tests.testCase03();
+//         tests.testCase04();
+
+//         // END Tests
+
+//         tests.endTest(); // End your test by clearning connections and closing browser
+//     }
+
+//     public static void main(String[] args) throws InterruptedException, MalformedURLException {
+//         new App().getGreeting();
+//     }
+// }
 package demo;
 import java.net.MalformedURLException;
 
@@ -5,6 +46,11 @@ import java.net.MalformedURLException;
 public class App {
     public void getGreeting() throws InterruptedException, MalformedURLException {
         System.out.println("Hello Autmation Wizards!");
+
+        
+
+
+
     }
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,34 +1,102 @@
+// package demo;
+
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeDriverService;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.logging.LogType;
+// import org.openqa.selenium.logging.LoggingPreferences;
+// import org.testng.annotations.AfterTest;
+// import org.testng.annotations.BeforeTest;
+// import org.testng.annotations.Test;
+
+// import java.util.logging.Level;
+// // import io.github.bonigarcia.wdm.WebDriverManager;
+// import demo.wrappers.Wrappers;
+
+// public class TestCases {
+//     ChromeDriver driver;
+
+//     /*
+//      * TODO: Write your tests here with testng @Test annotation. 
+//      * Follow `testCase01` `testCase02`... format or what is provided in instructions
+//      */
+
+//     /*
+//      * Do not change the provided methods unless necessary, they will help in automation and assessment
+//      */
+//     @BeforeTest
+//     public void startBrowser()
+//     {
+//         System.setProperty("java.util.logging.config.file", "logging.properties");
+
+//         // NOT NEEDED FOR SELENIUM MANAGER
+//         // WebDriverManager.chromedriver().timeout(30).setup();
+
+//         ChromeOptions options = new ChromeOptions();
+//         LoggingPreferences logs = new LoggingPreferences();
+
+//         logs.enable(LogType.BROWSER, Level.ALL);
+//         logs.enable(LogType.DRIVER, Level.ALL);
+//         options.setCapability("goog:loggingPrefs", logs);
+//         options.addArguments("--remote-allow-origins=*");
+
+//         System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+
+//         driver = new ChromeDriver(options);
+
+//         driver.manage().window().maximize();
+//     }
+
+//     @AfterTest
+//     public void endTest()
+//     {
+//         driver.close();
+//         driver.quit();
+
+//     }
+// }
+
 package demo;
 
+import org.checkerframework.checker.units.qual.s;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrappers = new Wrappers();
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow testCase01 testCase02... format or what is provided in instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,16 +110,71 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void testCase01() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+        // finding out web element for name
+        driver.get(
+                "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        System.out.println("Successfully opened URL.");
+        WebElement name = driver.findElement(By.xpath("//div[@class='rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd']//input"));
+        wrappers.sendKeys(name, "Crio Learner");
+        System.out.println("Successfully entered Crio Learner.");
+        // finding out web element for "Why are you practicing Automation?
+
+        WebElement practicing_Automation = driver.findElement(By.tagName("textarea"));
+        long epoch = System.currentTimeMillis();
+        wrappers.sendKeys(practicing_Automation, "I want to be the best QA Engineer! " + epoch);
+        WebElement exprience = driver.findElement(By.xpath("//div[@data-value='3 - 5']/div/div"));
+        wrappers.elementClick(exprience);
+
+        // Selecting a skill set using selectskill method. it's user defined
+        wrappers.selectSkill(driver, "Java");
+        wrappers.selectSkill(driver, "Selenium");
+        wrappers.selectSkill(driver, "TestNG");
+
+        // Selecting addressed learner
+        WebElement learnerAddressed = driver.findElement(By.xpath("//span[text()='Choose']//parent::div"));
+        wrappers.elementClick(learnerAddressed);
+        wrappers.selectAddressedBy(driver, "Ms");
+        Thread.sleep(2000);
+
+        // Putting a date before 7 days ago.
+        WebElement dateSelected = driver.findElement(By.xpath("//input[@type='date']"));
+        wrappers.sendKeys(dateSelected, wrappers.date7DaysAgo());
+
+        // Using list of string store the HH and mm and putting into respective field
+        List<String> time = wrappers.setTime();
+        WebElement hour = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+        wrappers.sendKeys(hour, "07");
+        WebElement minute = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
+        wrappers.sendKeys(minute, "30");
+
+        // Finding out submit button and clicking on that button
+        WebElement submitButton = driver.findElement(By.xpath("//div[@aria-label='Submit']"));
+        submitButton.click();
+
+        // Printing the success message
+        WebElement successMessage1 = driver
+                .findElement(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']"));
+        WebElement successMessage2 = driver.findElement(By.xpath("//a[text()='Submit another response']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOf(successMessage1));
+        System.out.println("First success message is " + successMessage1.getText());
+        System.out.println("Second success message is " + successMessage2.getText());
+
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
